08832e4b63873618bbca78b1d35c2e07a8c59112
Usernamenotavailable12 <129051734+Usernamenotavailable12@users.noreply.github.com>
2024-06-28 14:03:46 +0400

    Update amb-cookie.js

--- amb-cookie.js
+++ amb-cookie.js
@@ -19,7 +19,7 @@ function createCookieConsentBanner() {
 
     // Create the message paragraph
     const message = document.createElement('p');
-    message.innerHTML = 'საიტი იყენებს Cookie ფაილებს. დახურვის ღილაკზე დაჭერით თქვენ ეთანხმებით Cookie ფაილების გამოყენების წესებს და პირობებს. <a href="https://www.ambassadoribet.com/privacy" target="_blank">Cookie პოლიტიკა.</a>';
+    message.innerHTML = 'საიტი იყენებს Cookie ფაილებს. დახურვის ღილაკზე დაჭერით თქვენ ეთანხმებით Cookie ფაილების გამოყენების წესებს და პირობებს. <a href="https://www.ambassadoribet.com/cookie-policy" target="_blank">Cookie პოლიტიკა.</a>';
 
     // Create the accept button
     const button = document.createElement('button');
