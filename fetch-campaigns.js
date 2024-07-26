e1584f901a0154457a225b22da66706a7c645cc7
Usernamenotavailable12 <129051734+Usernamenotavailable12@users.noreply.github.com>
2024-07-26 13:02:58 +0400

    Update fetch-campaigns.js

--- fetch-campaigns.js
+++ fetch-campaigns.js
@@ -1,12 +1,14 @@
-  // Get the JSON data from the <script> tag
-  const jsonData = document.getElementById('ng-state').textContent;
-  const data = JSON.parse(jsonData);
+  document.addEventListener('DOMContentLoaded', (event) => {
+    // Get the JSON data from the <script> tag
+    const jsonData = document.getElementById('ng-state').textContent;
+    const data = JSON.parse(jsonData);
 
-  // Extract the campaignId
-  const campaigns = data['api-cache.state']['USER_CAMPAIGNS'];
-  for (const key in campaigns) {
-    if (campaigns.hasOwnProperty(key)) {
-      const campaignId = key.replace(/["\[\]]/g, '').split(',')[0];
-      document.body.classList.add(campaignId);
+    // Extract the campaignId
+    const userCampaigns = data['api-cache.state']['USER_CAMPAIGNS'];
+    for (const key in userCampaigns) {
+      if (userCampaigns.hasOwnProperty(key)) {
+        const campaignId = key.replace(/["\[\]]/g, '').split(',')[0];
+        document.body.classList.add(campaignId);
+      }
     }
-  }
+  });
